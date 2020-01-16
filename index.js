b656d415cc55afe185cac02ddb9a192b03614314
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-15 22:27:15 -0600

    updated index for routers

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+
+const sessionConfig = require("./database/sessionConfig");
 
 // Import Routers
+const unrestrictedRouter = require("./routers/unrestrictedRouter");
+const restrictedRouter = require("./routers/restrictedRouter");
+
+// Import Middleware
+const restrictedMiddleware = require("./middleware/restrictedMiddleware");
 
 const server = express();
 const port = 5000;
@@ -10,8 +18,11 @@ const port = 5000;
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 // Use Routers
+server.use("/unrestricted", unrestrictedRouter);
+server.use("/restricted", restrictedMiddleware, restrictedRouter);
 
 // Sanity Check
 
@@ -19,11 +30,17 @@ server.get("/", (req, res) => {
   res.json({ message: "I work" });
 });
 
+// No Route Handler
+
+server.use((req, res) => {
+  res.status(400).json({ error: "Route Does Not Exist" });
+});
+
 // Global Error Handling
 server.use((err, req, res, next) => {
   console.log(`Error: ${err}`);
 
-  res.status(err.status || 500).json({ error: "Something went wrong" });
+  res.status(err.status || 500).json({ err });
 });
 
 server.listen(port, () => {
