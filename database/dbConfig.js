4d2d5364d71addeede5ce3e55d4fd06fc7ed8ebb
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-20 01:00:24 -0600

    added .env

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,4 +1,5 @@
 const knex = require("knex");
 const knexfile = require("../knexfile");
+const environment = process.env.ENVIRONMENT || "development";
 
-module.exports = knex(knexfile.development);
+module.exports = knex(knexfile[environment]);
